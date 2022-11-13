03754cb087a82d52ce47ce3d67f86139e3b06101
so1n <qaz6803609@163.com>
2022-11-13 23:21:01 +0800

    Modify, Protobuf file

--- gen_rpc.sh
+++ gen_rpc.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # set venv
 export VENV_PREFIX=""
 if [ -d 'venv' ] ; then
@@ -13,25 +14,11 @@ echo 'use venv path:' ${VENV_PREFIX}
 target_p='grpc_example_common'
 # project proto path
 source_p='protos'
-# service
-service_list=("book" "user" "common")
-
 rm -r "${target_p:?}/${source_p:?}"*
-mkdir -p "${target_p:?}/${source_p:?}"
 
-for service in "${service_list[@]}"
-do
-  mkdir -p "${target_p:?}/${source_p:?}/${service:?}"
-  echo  "from proto file:" $source_p/"$service"/*.proto "gen proto py file to" $target_p/$source_p
-  ${VENV_PREFIX}python -m grpc_tools.protoc \
-    --mypy_grpc_out=./$target_p \
-    --mypy_out=./$target_p \
-    --python_out=./$target_p \
-    --grpc_python_out=./$target_p \
-    -I. \
-    $source_p/"$service"/*.proto
-
-  touch $target_p/$source_p/"$service"/__init__.py
-  # fix grpc tools bug
-  sed -i "s/from protos.$service import/from . import/" $target_p/$source_p/$service/*.py
-done
+${VENV_PREFIX}python -m grpc_tools.protoc \
+  --mypy_grpc_out=./ \
+  --mypy_out=./ \
+  --python_out=./ \
+  --grpc_python_out=./ \
+  -I protos $(find ./protos -name '*.proto')
